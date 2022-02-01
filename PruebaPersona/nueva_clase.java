ec281a39c7062f2ee132ad9c02d4ab993a9b5283
Guille <ffetisovv96@gmail.com>
2022-02-01 21:15:40 +0100

    primer commit

---
+++ PruebaPersona/nueva_clase.java
@@ -0,0 +1,10 @@
+package pruebapersona;
+
+public class nueva_clase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
