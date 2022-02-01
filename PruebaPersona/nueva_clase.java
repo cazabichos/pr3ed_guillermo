f46d9251bf7fcaf8984e8da981faa13a4dec2f04
Guille <ffetisovv96@gmail.com>
2022-02-01 21:28:02 +0100

    Modificacion_persona

--- PruebaPersona/nueva_clase.java
+++ PruebaPersona/nueva_clase.java
@@ -1,6 +1,6 @@
 package pruebapersona;
 
-public class nueva_clase {
+public class nueva_clase { /Este comentario tambíen forma parte de la práctica
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
